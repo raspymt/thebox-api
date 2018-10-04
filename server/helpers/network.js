d92d3532ec5eb506073b0df7e5661f9410e4a563
Yves-Marie Trublin <yvestrublin@protonmail.com>
2018-10-04 15:37:26 +0700

    Debug UTF8 Ssid name

--- server/helpers/network.js
+++ server/helpers/network.js
@@ -1,36 +1,24 @@
 const wpa_cli = require('../utils/wpa_cli')
 const iw = require('../utils/iw')
+const exec = require("child-process-promise").exec
 
 const interface = 'wlan0'
 const testChar = /(\\x[\d\w]{2})/g;
 
 const list = async () => {
   try {
-    // const buf = Buffer.from("My Tr\xe1\xba\xa7n", 'ascii');
-    console.log(Buffer.from('80', 'hex').toString('utf8'))
-
-
     let networks = await iw.getNetworks(interface)
     networks.sort((a, b) => {
       return b.signal - a.signal
     })
 
-    networks.map((e) => {
-
-      // while ((m = testChar.exec(e.ssid)) !== null) {
-      //   // This is necessary to avoid infinite loops with zero-width matches
-      //   if (m.index === testChar.lastIndex) {
-      //     testChar.lastIndex++;
-      //   }
-        
-      //   // The result can be accessed through the `m`-variable.
-      //   m.forEach((match) => {
-      //     // console.log(`${match.replace('\\x', '0x')} : ${String.fromCharCode(match.replace('\\x', '0x'))}`);
-      //     // console.log(`${decodeUTF16LE(match.replace('\\x', '0x'))} : ${String.fromCharCode(match.replace('\\x', '0x'))}`);
-      //   });
-      // }
-    })
-
+    for (var i = networks.length - 1; i >= 0; i--) {
+      let ssidUTF8 = await exec(`echo $'${networks[i].ssid}'`)
+      if(ssidUTF8.stderr === ''){
+        networks[i].ssid = ssidUTF8.stdout.replace('\n', '')
+      }
+    }
+    
     const status = await wpa_cli.getStatus(interface)
 
     return { networks, status }
